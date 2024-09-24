2e60d6aab011236ff716f124ef8056625fba6b5a
vlzcrz <velizcruzv@gmail.com>
2024-09-24 18:48:32 -0300

    se agregan los .env de deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SearchServiceModule } from './search-service/search-service.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/search-service'), SearchServiceModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), SearchServiceModule],
   controllers: [],
   providers: [],
 })
